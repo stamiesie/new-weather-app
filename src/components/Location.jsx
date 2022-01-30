0f35e61a15dfa18ec362b4ba14cca02f6b5815fd
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-01-30 10:50:40 -0800

    style Location component

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import styles from './Location.module.css';
 
 const Location = ({ town, temperature, description, icon }) => (
-  <div>
-    <img
-      src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-      alt={description}
-    />
-    <p>{town}</p>
-    <p>{Math.round(temperature)}&#176; F</p>
-    <p>{description}</p>
-    <p>{moment().format('dddd')}</p>
-    <p>{moment().format('LL')}</p>
+  <div className={styles.locationParent}>
+    <div className={styles.locationCard}>
+      <p>
+        {moment().format('dddd')} {moment().format('LL')}
+      </p>
+      <img
+        src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+        alt={description}
+      />
+      <p>Currently in {town}</p>
+      <p>{description}</p>
+      <p>{Math.round(temperature)}&#176; F</p>
+    </div>
   </div>
 );
 
